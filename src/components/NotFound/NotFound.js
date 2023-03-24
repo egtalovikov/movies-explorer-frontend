c8710afdab97e777e7819d82fe55112405935533
Egor Talovikov <egtalovikov@gmail.com>
2023-03-24 12:57:27 +0300

    feat: сделал функционал кнопки назад на странице 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import "./NotFound.css";
 
 function NotFound() {
+  let history = useHistory();
+
   return (
     <section className="not-found">
       <h1 className="not-found__title">404</h1>
       <p className="not-found__subtitle">Страница не найдена</p>
-      <Link to="#" className="not-found__back-link links">
+      <button onClick={() => history.goBack()} className="not-found__back-link buttons">
         Назад
-      </Link>
+      </button>
     </section>
   );
 }
